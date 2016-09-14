14aad31e9b710ba31ed9c0eee83303e957b6b6ff
William Silva <william.silva@uemg.br>
2016-09-14 09:14:25 -0300

    Adicionando a aplicação ao repositorio

---
+++ wordpress/wp-content/themes/meutema/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body <body <?php body_class(); ?>>
+	<header>
+		<h1><?php bloginfo('title'); ?></h1>
+	</header>
